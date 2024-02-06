c25f8ac7546e296a11fbc31e0f35664f3bd7b1d3
wience <wbdelafuente@up.edu.ph>
2024-02-06 14:56:58 +0800

    asd

--- WARMUP_PROJECT_KERNEL/boot.asm
+++ WARMUP_PROJECT_KERNEL/boot.asm
@@ -1,12 +1,21 @@
+mov ah, 0x0e
 mov al, 'A'
 int 0x10
+
+; inc al
+; add al, 0x20
+; int 0x10
 jmp loop
 
 loop:
     inc al
-    cmp al, 'Z' + 1
-    je exit
+    add al, 0x20
     int 0x10
+    sub al, 0x20
+    inc al
+    int 0x10
+    cmp al, 'z' + 1
+    jge exit
     jmp loop
 exit:
     jmp $
